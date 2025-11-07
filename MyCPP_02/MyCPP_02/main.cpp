429992fd92d9de7f8bf061ad8e242338cbc87aee
Shamsul Arifin <arifinbd6@gmail.com>
2025-11-07 15:37:35 +0600

    Added MyCPP_02 Visual Studio C++ project files

---
+++ MyCPP_02/MyCPP_02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int Add(int a, int b)
+{
+	return a + b;
+}
+
+void main()
+{
+	std::cout << "My Calculations" << std::endl;
+
+	int result = Add(1,2);
+	std::cout << "Integer Addition: 1 + 2 = " << result << std::endl;
+
+	result = Add(3, 4);
+	std::cout << "Integer Addition: 3 + 4 = " << result << std::endl;
+
+	std::cout << "Finished!";
+}
